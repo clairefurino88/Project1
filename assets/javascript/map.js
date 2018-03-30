87d4642f1101cc34e3f290960688812e9954e43d
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-03-30 11:05:18 -0400

    added css features

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,13 +1,25 @@
        // This api requires that you consent to location sharing when prompted by your browser. 
        //If you see the error "The Geolocation service failed.", it means you did not give permission 
        //for the browser to locate you.
-       var map, infoWindow,
+       var map
+       var infoWindow,
        markers = [];
-   function initMap() {
+       function initMap() { //initiate the map
+
+    //The JavaScript class that represents a map is the 'Map class'.
+    //I created a new instance of this class using the JavaScript new operator.
+    //When I created a new map instance, I specified a <div> HTML element in the page
+    //as a container which is 'map'. 
+    //HTML nodes are children of the JavaScript document object, 
+    //and we obtain a reference to this element via the document.getElementById() method.
+    //This code defines a variable (named map) and assigns that variable to a new Map object. 
+    //The function Map() is known as a constructor 
+
        map = new google.maps.Map(document.getElementById('map'), {
            center: { lat: 19.0759837, lng: 72.87765590000004 },
            zoom: 16
        });
+
        infoWindow = new google.maps.InfoWindow;
        if (navigator.geolocation) { //attempts to find current location of user
            navigator.geolocation.getCurrentPosition(function (position) {
@@ -18,7 +30,7 @@
                infoWindow.setPosition(pos); //if current location is found it will position 
                infoWindow.setContent('Location found.'); //the browser to be centered around that location
                infoWindow.open(map);
-               map.setCenter(pos);
+               map.setCenter(pos); //centers position of map
                locateGym(map, pos);
            }, function () { //if it cannot it will go to 'handleLocationError' and the user will recieve an error notification                    
                handleLocationError(true, infoWindow, map.getCenter());
@@ -27,11 +39,12 @@
            // Browser doesn't support Geolocation
            handleLocationError(false, infoWindow, map.getCenter());
        }
-       var input = document.getElementById('pac-input');
-       var searchBox = new google.maps.places.SearchBox(input);
+
+       var input = document.getElementById('pac-input'); //input information seen in div created in html element
+       var searchBox = new google.maps.places.SearchBox(input); //input of the search box
        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); //searchbox located in top left corner of map
        // Bias the SearchBox results towards current map's viewport.
-       map.addListener('bounds_changed', function () {
+       map.addListener('bounds_changed', function () { //after you search one location it lets you change the bounds
            searchBox.setBounds(map.getBounds());
        });
        searchBox.addListener('places_changed', function () { //recieves exact longitude and latitude of places
@@ -50,11 +63,11 @@
        });
    }
    function locateGym(map, pos) { //places marker at gyms location
-       places = new google.maps.places.PlacesService(map);
+       places = new google.maps.places.PlacesService(map); //uses google operator to find the location
        var markerIcon = {
            url: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX22676869.jpg',
            size: new google.maps.Size(60, 60),
-           origin: new google.maps.Point(0, 0),
+           origin: new google.maps.Point(0, 0), //google maps parameters for placing the icon image
            anchor: new google.maps.Point(12, 10),
            scaledSize: new google.maps.Size(30, 30)
        };                          //radius is in meter
@@ -82,8 +95,8 @@
        }
        markers = [];
    }
-   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-       infoWindow.setPosition(pos);
+   function handleLocationError(browserHasGeolocation, infoWindow, pos) {//if permission is not given or the location does not exist, 
+       infoWindow.setPosition(pos);                                        //this will be displayed
        infoWindow.setContent(browserHasGeolocation ?
            'Error: The Geolocation service failed.' :
            'Error: Your browser doesn\'t support geolocation.');
