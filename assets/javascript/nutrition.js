7b35822b995bce0da26195130eba79f1c963583c
alexbaranov43 <alexbaranov43@gmail.com>
2018-04-03 12:09:46 -0400

    final touches

--- assets/javascript/nutrition.js
+++ assets/javascript/nutrition.js
@@ -1,7 +1,16 @@
-
 //array of recipes to be generated as boxes and all searches will push to this array
 var recipes = ["grilled chicken", "garden salad", "tuna wrap", "farro", "beet salad"];
+var favorites = [];
 
+
+var fav =JSON.parse(localStorage.getItem("favorites"))
+if(fav !== null ){
+    
+
+for (i=0; i< fav.length; i++){
+    favorites.push(fav[i]);
+}}
+//$("#favorites").hide();
 // function to display search results / ajax request
 function displayRecipe() {
     //API Key: d685a1e4d3884f01da23f10c9b5c8603 
@@ -20,12 +29,14 @@ function displayRecipe() {
         var responses = response.recipes;
         // console.log(response);
         //for loop using i<10 to limit the search results to 10
-        for (var i = 0 ; i < 10; i++){ 
+        for (var i = 0; i < 10; i++) {
             console.log(responses[i])
             //create a new div for all the results to go into
             var recDiv = $("<div class='col-md-12' id='recDiv'>");
             //create an image
             var recipeImage = $("<img id='recipePic'><br>")
+            //create favorites button
+            var favoriteBtn = $("<button class='btn btn-success addFavorite'>Bookmark as Favorite</button>")
             //create a link to the actually recipe
             var recipeButton = $("<a href='' target='_blank' id='recipeWindow'></a>")
             //get the name of the dish
@@ -33,33 +44,105 @@ function displayRecipe() {
             recipeButton.attr("href", responses[i].source_url);
             recipeImage.attr("src", responses[i].image_url);
             recipeButton.text("View Recipe");
+            favoriteBtn.attr("href", responses[i].source_url);
+            favoriteBtn.attr("src", responses[i].image_url);
+            favoriteBtn.attr("title", responses[i].title);
             recDiv.prepend(title);
             recDiv.prepend(recipeButton);
             recDiv.prepend(recipeImage);
+            recDiv.prepend(favoriteBtn);
             $("#recipeResults").prepend(recDiv);
+            $("#recipeResults").show();
+            $("#favorites").hide();
         }
+
+            $(".addFavorite").on("click", function(event) {
+                event.preventDefault();
+                console.log(this)
+
+               
+                var favorite = {
+                    href: $(this).attr("href"), 
+                    src: $(this).attr("src"),
+                    title:$(this).attr("title")
+                }
+
+                favorites.push(favorite);
+                console.log(favorites)
+                localStorage.setItem("favorites", JSON.stringify(favorites))
+                console.log(favorite)
+
+               
+                    // event.preventDefault();
+                    // $("#favorites").show();
+            
+                    // $("#favorites").empty();
+
+                    // var favorites = JSON.parse(localStorage.getItem("favorites"));
+                    // for (i = 0; i < favorites.length; i++) {
+
+                    //     var favRecipe = $("recDiv, recipeImage, recipeButton, title")
+
+                    //     favRecipe.attr("src", favorites[i]);
+                    //     console.log(favRecipe.attr("src"));
+                    //     $("#favorites").prepend(favRecipe);
+                    // }
+
+
+                
+            })
+        
+
+
     })
 }
 
 //shows the recipes as boxes to click on
 function renderBox() {
     $("#recipeBox").empty();
-    for (i = 0; i < recipes.length; i++){
-    var box = $("<button class='btn btn-danger'>")
-    box.addClass("food")
-    box.attr("data-name", recipes[i]);
-    box.text(recipes[i]);
-    $("#recipeBox").append(box);}
+    for (i = 0; i < recipes.length; i++) {
+        var box = $("<button class='btn btn-danger'>")
+        box.addClass("food")
+        box.attr("data-name", recipes[i]);
+        box.text(recipes[i]);
+        $("#recipeBox").append(box);
+    }
 }
+
+$(".favorite").on("click", function(event){
+    event.preventDefault();
+   
+    $("#favorites").empty();
+    $("#recipeResults").hide();
+    $("#favorites").show();
+    var favorites = JSON.parse(localStorage.getItem("favorites"))
+    for(i=0; i < favorites.length; i++){
+
+    var bookmarkDiv = $("<div class='col-md-4 bookmarkDiv'>");
+    var recTitle = $("<div class='title'>");
+    var recImg = $("<img id='recipePic'><br>");
+    var recSrc = $("<a href='' target ='_blank' id='recipeWindow'>");
+
+    recImg.attr("src", favorites[i].src);
+    recTitle.text(favorites[i].title);
+    recSrc.attr("href", favorites[i].href);
+    recSrc.text("View Recipe");
+
+    bookmarkDiv.prepend(recTitle);
+    bookmarkDiv.prepend(recSrc);
+    bookmarkDiv.prepend(recImg);
+    $("#favorites").prepend(bookmarkDiv);
+    }
+})
 
 //pushes the search to the recipe array
 $("#search").on("click", function (event) {
     event.preventDefault();
     var recipe = $("#newRecipe").val().trim()
 
-         if (recipe === "") {
-            return;
-        }
+    if (recipe === "") {
+        return;
+    }
     recipes.push(recipe);
     $("#newRecipe").val("")
     renderBox();
@@ -68,3 +151,7 @@ $("#search").on("click", function (event) {
 //displays the results when the box is clicked
 $(document).on("click", ".food", displayRecipe);
 renderBox();
+
+$("#recipeResults").show();
+$("#favorites").hide();
+
